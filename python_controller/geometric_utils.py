93217a90aca4d51d9b8d6395893d1b4382f680a7
Emanuele Fabbiani <donlelef@gmail.com>
2017-03-20 20:59:35 +0100

    Bug1 implemention started. Robot shows an unexpected behaviour.

--- python_controller/geometric_utils.py
+++ python_controller/geometric_utils.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 from position import Position2D
+import math
 
 class GeometricUtils:
 
@@ -10,4 +11,4 @@ class GeometricUtils:
         self.tolerance = tolerance
 
     def check_if_collinear(self, p1, p2, p3):
-        return (p2.y - p1.y) * (p3.x - p2.x) - (p3.y - p2.y) * (p2.x - p1.x) < self.tolerance
+        return abs((p2.y - p1.y) * (p3.x - p2.x) - (p3.y - p2.y) * (p2.x - p1.x)) < self.tolerance
--- python_controller/robot_navigation.py
+++ python_controller/robot_navigation.py
@@ -15,6 +15,19 @@ from robot import Robot
 from position import Position2D
 from trilateration_solver import TrilaterationSolver
 from geometric_utils import GeometricUtils
+import math
+
+
+def ahead_not_free(robot):
+    return robot.ahead_range < 2
+
+
+def left_too_far(robot):
+    return robot.at_left_range > 1.5
+
+
+def left_too_near(robot):
+    return robot.at_left_range < 1
 
 try:
     from pymorse import Morse
@@ -22,24 +35,62 @@ except ImportError:
     print("you need first to install pymorse, the Python bindings for MORSE!")
     sys.exit(1)
 
+
 with Morse() as sim:
 
     robot = Robot(sim.robot)
-    solver = TrilaterationSolver(0.1)
+    solver = TrilaterationSolver(0.01)
 
     pos1 = robot.position
     r1 = robot.distance_to_target
 
-    robot.set_velocity(1, 0).stop_after(1)
+    robot.set_velocity(1, 1).stop_after(0.5)
 
     pos2 = robot.position
     r2 = robot.distance_to_target
-    
-    robot.set_velocity(1, 1).stop_after(1)
+
+    robot.set_velocity(1, 1).stop_after(0.5)
 
     pos3 = robot.position
     r3 = robot.distance_to_target
 
-    goal = solver.trilaterate_using_projections(pos1.x, pos1.y, r1, pos2.x, pos2.y, r2, pos3.x, pos3.y, r3)
+    goal = solver.trilaterate_using_projections(
+        pos1.x, pos1.y, r1, pos2.x, pos2.y, r2, pos3.x, pos3.y, r3)
+
+    state = 'motion_to_goal'
+
+    while robot.distance_to_target > 3:
+        if state == 'motion_to_goal':
+            actual_position = pos3
+            actual_orientation = robot.orientation
+            vector_to_goal = goal - actual_position
+            goal_orientation = math.atan2(vector_to_goal.y, vector_to_goal.x)
+            required_rotation_angle = goal_orientation - actual_orientation
+
+            robot.rotate_of(required_rotation_angle, 1)
+            robot.set_velocity(100, 0)
+
+            while (robot.ahead_range >= 2) & (robot.distance_to_target > 3):
+                pass
+
+            robot.stop()
+            state = 'boudary_following'
+            break
+
+        if state == 'boudary_following':
+            while True:
+                if ahead_not_free(robot):
+                    robot.set_velocity(0, -2)
+                    while ahead_not_free(robot):
+                        pass
+                elif left_too_near(robot):
+                    robot.set_velocity(2, -2)
+                    while left_too_near(robot):
+                        pass
+                elif left_too_far(robot):
+                    robot.set_velocity(2, 2)
+                    while left_too_far(robot):
+                        pass
+            robot.set_velocity(2, 0)
     
-    print(str(goal))
+    robot.stop()
