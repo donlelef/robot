84efd88c8bea0a1c4946a962b498d1002aebd36b
simone.colucci <simone.colucci@techedgegroup.com>
2017-03-26 16:18:33 +0200

    Algorithm changed Changed implementation of mtg route re-computation to avoid drift in case of long distances

--- python_controller/motion_to_goal.py
+++ python_controller/motion_to_goal.py
@@ -1,17 +1,16 @@
 import math
 import time
+from threading import Timer
 
 class MotionToGoal(object):
+
+    TIME_OF_DRIFT = 2
 
     robot = None
     goal = None
     motion_velocity = None
     rotation_velocity = None
-
-    def _rotate_to_goal(self):
-        vector_to_goal = self.goal - self.robot.position
-        goal_orientation = math.atan2(vector_to_goal.y, vector_to_goal.x)
-        self.robot.rotate_to(goal_orientation, self.rotation_velocity)
+    _route_recompute = True
 
     def __init__(self, robot, goal, motion_velocity = 2, rot_velocity = 2):
         self.robot = robot
@@ -23,8 +22,17 @@ class MotionToGoal(object):
         self.robot.stop()
         print("Straaaaaight to the target, sir!")
         while self.robot.free_space_ahead >= self.robot.SENSORS_RANGE and self.robot.distance_to_target >= 2:
-            self._rotate_to_goal()
-            self.robot.set_velocity(self.motion_velocity, 0)
-            time.sleep(0.5)
-            self.robot.stop()
+            if self._route_recompute:
+                self.robot.stop()
+                self._rotate_to_goal()
+                self._route_recompute = False
+                Timer(self.TIME_OF_DRIFT, self._set_recompute_flag).start()
+                self.robot.set_velocity(self.motion_velocity, 0)
 
+    def _rotate_to_goal(self):
+        vector_to_goal = self.goal - self.robot.position
+        goal_orientation = math.atan2(vector_to_goal.y, vector_to_goal.x)
+        self.robot.rotate_to(goal_orientation, self.rotation_velocity)
+
+    def _set_recompute_flag():
+        self._route_recompute = True
